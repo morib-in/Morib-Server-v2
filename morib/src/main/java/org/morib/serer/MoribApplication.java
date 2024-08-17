63eb0633807a82e892966c29a4d06c2faa7de535
geniuus <cross_man@naver.com>
2024-08-17 17:26:05 +0900

    Feat [#1] springboot 프로젝트 생성

---
+++ morib/src/main/java/org/morib/serer/MoribApplication.java
@@ -0,0 +1,13 @@
+package org.morib.serer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoribApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoribApplication.class, args);
+    }
+
+}
---
+++ morib/src/test/java/org/morib/serer/MoribApplicationTests.java
@@ -0,0 +1,13 @@
+package org.morib.serer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoribApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
