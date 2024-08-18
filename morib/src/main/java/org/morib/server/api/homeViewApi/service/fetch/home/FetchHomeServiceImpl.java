f5112e2f071f5841c8d1e38874956ee311b07422
geniuus <cross_man@naver.com>
2024-08-18 21:12:23 +0900

    Feat [#6] 도메인 삭제, Gateway 도입 (Skltn)

--- morib/src/main/java/org/morib/server/api/homeViewApi/service/fetch/home/FetchHomeServiceImpl.java
+++ morib/src/main/java/org/morib/server/api/homeViewApi/service/fetch/home/FetchHomeServiceImpl.java
@@ -1,25 +1,22 @@
 package org.morib.server.api.homeViewApi.service.fetch.home;
 
 import lombok.RequiredArgsConstructor;
-import org.morib.server.domain.category.CategoryReader;
-import org.morib.server.domain.task.TaskReader;
-import org.morib.server.domain.timer.TimerReader;
+import org.morib.server.domain.category.infra.CategoryGateway;
+import org.morib.server.domain.task.infra.TaskGateway;
+import org.morib.server.domain.timer.infra.TimerGateway;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class FetchHomeServiceImpl implements FetchHomeService {
 
-    private final CategoryReader categoryReader;
-    private final TaskReader taskReader;
-    private final TimerReader timerReader;
+    private final CategoryGateway categoryGateway;
+    private final TaskGateway taskGateway;
+    private final TimerGateway timerGateway;
 
     @Override
     public void execute() {
-        categoryReader.fetchCategories();
-        taskReader.fetch();
-        timerReader.fetch();
+        // 각 Gateway로 엔티티 조회 후 Dto Build
     }
-
 
 }
