7b3643fcff6eb2d33f998b443dc7379ab4509e1e
geniuus <cross_man@naver.com>
2024-08-18 21:15:33 +0900

    Feat [#6] Category Entity에 Getter 추가
    dto build 시 필요

--- morib/src/main/java/org/morib/server/domain/category/infra/Category.java
+++ morib/src/main/java/org/morib/server/domain/category/infra/Category.java
@@ -1,8 +1,10 @@
 package org.morib.server.domain.category.infra;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
