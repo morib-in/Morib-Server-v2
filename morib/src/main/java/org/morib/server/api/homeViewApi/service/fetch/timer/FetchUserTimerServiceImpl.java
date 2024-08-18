83b33cd3a6c922406c6d93a53aee84d6a9df5ea9
geniuus <cross_man@naver.com>
2024-08-18 21:13:51 +0900

    Feat [#6] 도메인 삭제, Gateway 도입 (Skltn)

--- morib/src/main/java/org/morib/server/api/homeViewApi/service/fetch/timer/FetchUserTimerServiceImpl.java
+++ morib/src/main/java/org/morib/server/api/homeViewApi/service/fetch/timer/FetchUserTimerServiceImpl.java
@@ -1,22 +1,22 @@
 package org.morib.server.api.homeViewApi.service.fetch.timer;
 
 import lombok.RequiredArgsConstructor;
-import org.morib.server.domain.task.TaskReader;
-import org.morib.server.domain.timer.TimerAggregator;
-import org.morib.server.domain.timer.TimerReader;
+import org.morib.server.domain.task.infra.TaskGateway;
+import org.morib.server.domain.timer.TimerOperator;
+import org.morib.server.domain.timer.infra.TimerGateway;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class FetchUserTimerServiceImpl implements FetchUserTimerService{
-    private final TaskReader taskReader;
-    private final TimerReader timerReader;
-    private final TimerAggregator timerAggregator;
+    private final TaskGateway taskGateway;
+    private final TimerGateway timerGateway;
+    private final TimerOperator timerOperator;
 
     @Override
     public void execute() {
-        taskReader.fetch();
-        timerReader.fetch();
-        timerAggregator.aggregate();
+        taskGateway.fetchTasksInRange();
+        timerGateway.fetchTimer();
+        timerOperator.aggregate();
     }
 }
