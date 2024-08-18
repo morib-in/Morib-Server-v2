22e8ed93c77d17ac6dc2a172b3ef6faecc2a262e
Khyojun <khyojun1104@gmail.com>
2024-08-18 22:46:40 +0900

    Feat [#5] Facade 관련 어노테이션 생성
    facade를 사용하는데 component로도 사용할 수 있을 것 같지만, facade라는 키워드가 붙어서 component를 facade라는 어노테이션을 진행해보면 어떨까 생각해서 작업해봤습니다.

---
+++ morib/src/main/java/org/morib/server/annotation/Facade.java
@@ -0,0 +1,17 @@
+package org.morib.server.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Indexed;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+@Indexed
+public @interface Facade {
+
+}
