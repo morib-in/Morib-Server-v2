f96a64076781e4d7878fbb4fbc3b6d1a9138b4d1
geniuus <cross_man@naver.com>
2024-08-18 21:16:08 +0900

    Feat [#6] Operator 도입
    특정 기능을 수행하는 Aggregator 말고, 도메인 로직만을 수행하는 Operator 도입

---
+++ morib/src/main/java/org/morib/server/domain/task/TaskOperator.java
@@ -0,0 +1,9 @@
+package org.morib.server.domain.task;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TaskOperator {
+}
--- morib/src/main/java/org/morib/server/domain/timer/TimerOperator.java
+++ morib/src/main/java/org/morib/server/domain/timer/TimerOperator.java
@@ -1,11 +1,11 @@
 package org.morib.server.domain.timer;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
 @RequiredArgsConstructor
-public class TimerAggregator {
+@Service
+public class TimerOperator {
 
     public void aggregate() {
         // 각 task의 timer들을 종합해 오늘 나의 작업시간 계산
