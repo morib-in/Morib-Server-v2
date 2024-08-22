58c8a18c77ffa322425323457edd5261c68e66e0
geniuus <cross_man@naver.com>
2024-08-22 20:42:08 +0900

    Refactor [#9] 인터페이스 분리로 인한 네이밍 변경

---
+++ morib/src/main/java/org/morib/server/api/homeViewApi/service/create/todo/CreateTodoServiceImpl.java
@@ -0,0 +1,16 @@
+package org.morib.server.api.homeViewApi.service.create.todo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CreateTodoServiceImpl implements CreateTodoService{
+//    private final TodoGateway todoGateway;
+
+    @Override
+    public void create() {
+
+    }
+
+}
