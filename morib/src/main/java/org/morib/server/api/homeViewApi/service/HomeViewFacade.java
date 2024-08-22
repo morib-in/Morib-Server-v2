f54bbcb7c93ae8faf00927af8aa8143ec100e8aa
geniuus <cross_man@naver.com>
2024-08-22 20:49:51 +0900

    Feat [#9] 홈뷰 전체조회 Facade
    각 Service를 call해서 로직을 사용하고, Dto를 빌드하는 역할을 합니다.

--- morib/src/main/java/org/morib/server/api/homeViewApi/service/HomeViewFacade.java
+++ morib/src/main/java/org/morib/server/api/homeViewApi/service/HomeViewFacade.java
@@ -1,26 +1,64 @@
 package org.morib.server.api.homeViewApi.service;
 
 import lombok.RequiredArgsConstructor;
-import org.morib.server.api.homeViewApi.service.create.CreateTaskService;
-import org.morib.server.api.homeViewApi.service.fetch.home.FetchHomeService;
-import org.morib.server.api.homeViewApi.service.fetch.timer.FetchUserTimerService;
-import org.morib.server.api.homeViewApi.service.start.StartTimerService;
-import org.morib.server.api.homeViewApi.service.toggle.ToggleTaskStatusService;
-import org.springframework.stereotype.Component;
+import org.morib.server.api.homeViewApi.dto.fetch.*;
+import org.morib.server.api.homeViewApi.service.aggregate.timer.AggregateTimerService;
+import org.morib.server.api.homeViewApi.service.classify.category.ClassifyCategoryService;
+import org.morib.server.api.homeViewApi.service.classify.task.ClassifyTaskService;
+import org.morib.server.api.homeViewApi.service.classify.timer.ClassifyTimerService;
+import org.morib.server.api.homeViewApi.service.create.task.CreateTaskService;
+import org.morib.server.api.homeViewApi.service.create.timer.CreateTimerService;
+import org.morib.server.api.homeViewApi.service.create.todo.CreateTodoService;
+import org.morib.server.api.homeViewApi.service.fetch.category.FetchCategoryService;
+import org.morib.server.api.homeViewApi.service.fetch.task.FetchTaskService;
+import org.morib.server.api.homeViewApi.service.fetch.timer.FetchTimerService;
+import org.morib.server.api.homeViewApi.service.toggle.task.ToggleTaskStatusService;
+import org.morib.server.domain.category.infra.Category;
+import org.morib.server.domain.task.infra.Task;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class HomeViewFacade {
-
-    private final FetchHomeService fetchHomeService;
-    private final FetchUserTimerService fetchUserTimerService;
+    private final FetchCategoryService fetchCategoryService;
+    private final ClassifyCategoryService classifyCategoryService;
+    private final FetchTaskService fetchTaskService;
+    private final ClassifyTaskService classifyTaskService;
+    private final FetchTimerService fetchTimerService;
+    private final ClassifyTimerService classifyTimerService;
+    private final AggregateTimerService aggregateTimerService;
     private final CreateTaskService createTaskService;
     private final ToggleTaskStatusService toggleTaskStatusService;
-    private final StartTimerService startTimerService;
+    private final CreateTimerService createTimerService;
+    private final CreateTodoService createTodoService;
 
-    public void fetchHome() {
-        fetchHomeService.execute();
+    public List<HomeViewResponseDto> fetchHome(HomeViewRequestDto request) {
+        Map<LocalDate, List<Category>> categories = classifyCategoryService.classifyByDate(fetchCategoryService.fetch(request), request.startDate(), request.endDate());
+        Map<LocalDate, Map<Category, List<Task>>> combined = classifyCategoryService.classifyTaskByCategory(categories);
+        Map<LocalDate, List<FetchCombinedDto>> combinedDto = combined.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue().entrySet().stream()
+                                .map(categoryEntry -> {
+                                    FetchCategoryDto categoryDto = FetchCategoryDto.of(categoryEntry.getKey());
+                                    List<FetchTaskDto> taskDtos = categoryEntry.getValue().stream()
+                                            .map(task -> {
+                                                int elapsedTime = classifyTaskService.classifyTimerByTask(entry.getKey(), task);
+                                                return FetchTaskDto.of(task, elapsedTime);
+                                            })
+                                            .collect(Collectors.toList());
+                                    return FetchCombinedDto.of(categoryDto, taskDtos);
+                                })
+                                .collect(Collectors.toList())
+                ));
+        return combinedDto.entrySet().stream()
+                .map(entry -> HomeViewResponseDto.of(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
     }
 
     public void fetchUserTimer() {
