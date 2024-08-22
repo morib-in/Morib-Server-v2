e79a806d77758abfcf59700e83ed9a5d784a305b
geniuus <cross_man@naver.com>
2024-08-22 20:43:50 +0900

    Feat [#9] Manager 어노테이션
    도메인 로직을 담고 있는 Manager 어노테이션 구현했습니다.

--- morib/src/main/java/org/morib/server/annotation/Manager.java
+++ morib/src/main/java/org/morib/server/annotation/Manager.java
@@ -1,2 +1,17 @@
-package org.morib.server.annotation;public interface Manager {
+package org.morib.server.annotation;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Indexed;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+@Indexed
+public @interface Manager {
+
 }
