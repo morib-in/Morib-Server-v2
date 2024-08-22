c3bbae7436b6e1c0d96d824be6ee643f5de5223e
geniuus <cross_man@naver.com>
2024-08-22 20:45:31 +0900

    Feat [#9] 홈뷰에서 사용되는 Service, 구현체
    Classify~ 네이밍을 사용해서 분류한다는 느낌을 내봤습니다.

--- morib/src/main/java/org/morib/server/api/homeViewApi/service/classify/category/ClassifyCategoryService.java
+++ morib/src/main/java/org/morib/server/api/homeViewApi/service/classify/category/ClassifyCategoryService.java
@@ -1,2 +1,13 @@
-package org.morib.server.api.homeViewApi.service.classify.category;public interface ClassifyCategoryService {
+package org.morib.server.api.homeViewApi.service.classify.category;
+
+import org.morib.server.domain.category.infra.Category;
+import org.morib.server.domain.task.infra.Task;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public interface ClassifyCategoryService {
+    Map<LocalDate, List<Category>> classifyByDate(List<Category> categories, LocalDate startDate, LocalDate endDate);
+    Map<LocalDate, Map<Category, List<Task>>> classifyTaskByCategory(Map<LocalDate, List<Category>> categories);
 }
--- morib/src/main/java/org/morib/server/api/homeViewApi/service/classify/timer/ClassifyTimerServiceImpl.java
+++ morib/src/main/java/org/morib/server/api/homeViewApi/service/classify/timer/ClassifyTimerServiceImpl.java
@@ -1,2 +1,23 @@
-package org.morib.server.api.homeViewApi.service.classify.timer;public class ClassifyTimerServiceImpl {
+package org.morib.server.api.homeViewApi.service.classify.timer;
+
+import lombok.RequiredArgsConstructor;
+import org.morib.server.domain.category.infra.Category;
+import org.morib.server.domain.task.infra.Task;
+import org.morib.server.domain.timer.TimerManager;
+import org.morib.server.domain.timer.infra.Timer;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+public class ClassifyTimerServiceImpl implements ClassifyTimerService{
+    private final TimerManager timerManager;
+
+    @Override
+    public Map<Task, Timer> classifyByTask(List<Task> tasks) {
+        timerManager.classifyByTask();
+    }
 }
