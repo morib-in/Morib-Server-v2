dbe5e47a210120d32d64a705dbc7e5b649bfd0ee
geniuus <cross_man@naver.com>
2024-08-22 20:46:58 +0900

    Feat [#9] TaskManager 구현
    Task 관련 도메인 로직을 담는 Manager입니다.

--- morib/src/main/java/org/morib/server/domain/task/TaskManager.java
+++ morib/src/main/java/org/morib/server/domain/task/TaskManager.java
@@ -1,9 +1,20 @@
 package org.morib.server.domain.task;
 
 import lombok.RequiredArgsConstructor;
+import org.morib.server.domain.task.infra.Task;
+import org.morib.server.domain.timer.infra.Timer;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @RequiredArgsConstructor
 @Service
-public class TaskOperator {
+public class TaskManager {
+    public int classifyTimerByTask(LocalDate date, Task task) {
+        return task.getTimers().stream()
+                .filter(timer -> timer.getTargetDate().equals(date))
+                .map(Timer::getElapsedTime)
+                .findAny()
+                .orElse(0); // 일치하는 Timer가 없으면 0을 반환
+    }
 }
