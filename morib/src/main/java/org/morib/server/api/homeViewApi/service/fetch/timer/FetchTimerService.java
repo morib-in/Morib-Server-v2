f7283ef9fa58e2e0a824f000755e70c80952935c
geniuus <cross_man@naver.com>
2024-08-24 00:52:31 +0900

    Refactor [#9] Timer 관련 패키지 이동 + Fix #15 conflict resolve
    application으로 Service 이동

--- morib/src/main/java/org/morib/server/api/homeViewApi/service/fetch/timer/FetchTimerService.java
+++
@@ -1,7 +0,0 @@
-package org.morib.server.api.homeViewApi.service.fetch.timer;
-
-import java.time.LocalDate;
-
-public interface FetchTimerService {
-    void fetch();
-}
--- morib/src/main/java/org/morib/server/domain/timer/application/ClassifyTimerServiceImpl.java
+++ morib/src/main/java/org/morib/server/domain/timer/application/ClassifyTimerServiceImpl.java
@@ -1,15 +1,11 @@
-package org.morib.server.api.homeViewApi.service.classify.timer;
+package org.morib.server.domain.timer.application;
 
 import lombok.RequiredArgsConstructor;
-import org.morib.server.domain.category.infra.Category;
 import org.morib.server.domain.task.infra.Task;
 import org.morib.server.domain.timer.TimerManager;
-import org.morib.server.domain.timer.infra.Timer;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -17,7 +13,7 @@ public class ClassifyTimerServiceImpl implements ClassifyTimerService{
     private final TimerManager timerManager;
 
     @Override
-    public Map<Task, Timer> classifyByTask(List<Task> tasks) {
-        timerManager.classifyByTask();
+    public int classifyTaskByDate(LocalDate date, Task task) {
+        return 0;
     }
 }
