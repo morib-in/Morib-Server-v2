6992372e277f6b0d8008138a1209f7ced45a828f
geniuus <cross_man@naver.com>
2024-08-24 02:35:04 +0900

    Refactor [#9] 리뷰에서 말씀하신 메서드 분리

--- morib/src/main/java/org/morib/server/api/homeViewApi/service/HomeViewFacade.java
+++ morib/src/main/java/org/morib/server/api/homeViewApi/service/HomeViewFacade.java
@@ -3,16 +3,19 @@ package org.morib.server.api.homeViewApi.service;
 import lombok.RequiredArgsConstructor;
 import org.morib.server.api.homeViewApi.dto.fetch.*;
 import org.morib.server.api.homeViewApi.service.aggregate.timer.AggregateTimerService;
-import org.morib.server.api.homeViewApi.service.classify.category.ClassifyCategoryService;
-import org.morib.server.api.homeViewApi.service.classify.task.ClassifyTaskService;
-import org.morib.server.api.homeViewApi.service.classify.timer.ClassifyTimerService;
+import org.morib.server.api.homeViewApi.vo.CategoriesByDate;
+import org.morib.server.api.homeViewApi.vo.CombinedByDate;
+import org.morib.server.api.homeViewApi.vo.TaskWithElapsedTime;
+import org.morib.server.domain.category.application.ClassifyCategoryService;
+import org.morib.server.domain.task.application.ClassifyTaskService;
+import org.morib.server.domain.timer.application.ClassifyTimerService;
 import org.morib.server.api.homeViewApi.service.create.task.CreateTaskService;
 import org.morib.server.api.homeViewApi.service.create.timer.CreateTimerService;
 import org.morib.server.api.homeViewApi.service.create.todo.CreateTodoService;
-import org.morib.server.api.homeViewApi.service.fetch.category.FetchCategoryService;
-import org.morib.server.api.homeViewApi.service.fetch.task.FetchTaskService;
-import org.morib.server.api.homeViewApi.service.fetch.timer.FetchTimerService;
-import org.morib.server.api.homeViewApi.service.toggle.task.ToggleTaskStatusService;
+import org.morib.server.domain.category.application.FetchCategoryService;
+import org.morib.server.domain.task.application.FetchTaskService;
+import org.morib.server.domain.timer.application.FetchTimerService;
+import org.morib.server.domain.task.application.ToggleTaskStatusService;
 import org.morib.server.domain.category.infra.Category;
 import org.morib.server.domain.task.infra.Task;
 import org.springframework.stereotype.Service;
@@ -38,25 +41,49 @@ public class HomeViewFacade {
     private final CreateTodoService createTodoService;
 
     public List<HomeViewResponseDto> fetchHome(HomeViewRequestDto request) {
-        Map<LocalDate, List<Category>> categories = classifyCategoryService.classifyByDate(fetchCategoryService.fetch(request), request.startDate(), request.endDate());
-        Map<LocalDate, Map<Category, List<Task>>> combined = classifyCategoryService.classifyTaskByCategory(categories);
-        Map<LocalDate, List<FetchCombinedDto>> combinedDto = combined.entrySet().stream()
+        List<CategoriesByDate> categories = classifyCategoryService.classifyByDate(
+                fetchCategoryService.fetchByUserIdInRange(request.userId(), request.startDate(), request.endDate()),
+                request.startDate(), request.endDate());
+        List<CombinedByDate> combined = classifyCategoryService.classifyTaskByCategory(categories);
+
+        List<TaskWithElapsedTime> tasks = combined.stream()
+                .flatMap(combinedByDate -> combinedByDate.getCombined().stream()
+                .flatMap(tasksByCategory -> classifyTaskService.classifyTimerByTask(combinedByDate.getDate(), tasksByCategory.getTasks()).stream()
+                )).toList();
+
+        return convertToHomeViewResponseDto(createFetchCombinedDtoMap(combined, tasks));
+    }
+
+    private Map<LocalDate, List<FetchCombinedDto>> createFetchCombinedDtoMap(
+            List<CombinedByDate> combined, List<TaskWithElapsedTime> tasks) {
+        return combined.stream()
                 .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> entry.getValue().entrySet().stream()
-                                .map(categoryEntry -> {
-                                    FetchCategoryDto categoryDto = FetchCategoryDto.of(categoryEntry.getKey());
-                                    List<FetchTaskDto> taskDtos = categoryEntry.getValue().stream()
-                                            .map(task -> {
-                                                int elapsedTime = classifyTaskService.classifyTimerByTask(entry.getKey(), task);
-                                                return FetchTaskDto.of(task, elapsedTime);
-                                            })
-                                            .collect(Collectors.toList());
+                        CombinedByDate::getDate,
+                        combinedByDate -> combinedByDate.getCombined().stream()
+                                .map(entry -> {
+                                    FetchCategoryDto categoryDto = FetchCategoryDto.of(entry.getCategory());
+                                    List<FetchTaskDto> taskDtos = createFetchTaskDtoList(entry.getTasks(), tasks);
                                     return FetchCombinedDto.of(categoryDto, taskDtos);
                                 })
-                                .toList()
+                                .collect(Collectors.toList())
                 ));
-        return combinedDto.entrySet().stream()
+    }
+
+    private List<FetchTaskDto> createFetchTaskDtoList(List<Task> tasks, List<TaskWithElapsedTime> taskWithElapsedTimes) {
+        return tasks.stream()
+                .map(task -> {
+                    int elapsedTime = taskWithElapsedTimes.stream()
+                            .filter(taskWithElapsedTime -> taskWithElapsedTime.getTask().equals(task))
+                            .map(TaskWithElapsedTime::getElapsedTime)
+                            .findFirst()
+                            .orElse(0);
+                    return FetchTaskDto.of(task, elapsedTime);
+                })
+                .collect(Collectors.toList());
+    }
+
+    private List<HomeViewResponseDto> convertToHomeViewResponseDto(Map<LocalDate, List<FetchCombinedDto>> combinedDtoMap) {
+        return combinedDtoMap.entrySet().stream()
                 .map(entry -> HomeViewResponseDto.of(entry.getKey(), entry.getValue()))
                 .collect(Collectors.toList());
     }
