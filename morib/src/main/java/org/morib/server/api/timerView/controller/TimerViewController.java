d168f1f72f41e6644ab95d27881ac6431a224287
Khyojun <nandong5923@naver.com>
2024-08-23 18:00:51 +0900

    Feat [#15] controller, dto 형태 일부 엔티티 변화에 따른 변경 및 타입에 맞게 구현
    - user의 경우 일단 주석으로 처리하고 뒤에서 mockUserId로 처리해서 진행

--- morib/src/main/java/org/morib/server/api/timerView/controller/TimerViewController.java
+++ morib/src/main/java/org/morib/server/api/timerView/controller/TimerViewController.java
@@ -3,6 +3,8 @@ package org.morib.server.api.timerView.controller;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.morib.server.api.timerView.dto.StopTimerRequestDto;
+import org.morib.server.api.timerView.dto.TodoCardResponseDto;
+import org.morib.server.api.timerView.service.fetch.todo.FetchTodoFacade;
 import org.morib.server.api.timerView.service.stop.timer.StopTimerFacade;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TimerViewController {
 
     private final StopTimerFacade stopTimerFacade;
-    //private final TodoFacade todoFacade; 이후 만들 예정
+    private final FetchTodoFacade fetchTodoFacade;
 
     @PostMapping("/timer/stop/{taskId}")
     public ResponseEntity<String> stopTimerAndFetchAccumulatedTime( // @AuthenticationPrincipal Long userId,
@@ -31,9 +33,9 @@ public class TimerViewController {
 
 
     @GetMapping("/timer/todo-card")
-    public ResponseEntity getTodoCards(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate targetDate) {
-       // todoFacade.getTodoCard(targetDate);
-        return ResponseEntity.ok("void");
+    public ResponseEntity<TodoCardResponseDto> getTodoCards(// @AuthenticationPrincipal Long userId,
+                                                            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate targetDate) {
+        return ResponseEntity.ok(fetchTodoFacade.fetchTodoCard(targetDate));
     }
 
 
