f1dabefcc1f98e506829de376f21d6e46fa7cc78
Khyojun <khyojun1104@gmail.com>
2024-08-23 23:19:21 +0900

    Refactor [#13] facade 관련하여 너무 자세하게 나누지 않기로 한 리뷰 반영을 위한 삭제

--- morib/src/main/java/org/morib/server/api/timerView/service/stop/timer/StopTimerFacade.java
+++
@@ -1,8 +0,0 @@
-package org.morib.server.api.timerView.service.stop.timer;
-
-import org.morib.server.api.timerView.dto.StopTimerRequestDto;
-
-public interface StopTimerFacade {
-
- void afterStopTimer(Long taskId, StopTimerRequestDto dto);
-}
