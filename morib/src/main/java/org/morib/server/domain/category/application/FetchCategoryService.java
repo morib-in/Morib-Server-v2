e3680a6756e93778f3db2bcce7ec6c773f7f7cf1
geniuus <cross_man@naver.com>
2024-08-25 16:33:13 +0900

    Refactor [#23] CategoryWithTasks 빌딩 코드 구현

--- morib/src/main/java/org/morib/server/domain/category/application/FetchCategoryService.java
+++ morib/src/main/java/org/morib/server/domain/category/application/FetchCategoryService.java
@@ -1,10 +1,17 @@
 package org.morib.server.domain.category.application;
 
+import org.morib.server.api.homeView.vo.CategoryWithTasks;
+import org.morib.server.api.homeView.vo.TaskWithTimers;
 import org.morib.server.domain.category.infra.Category;
+import org.morib.server.domain.task.infra.Task;
 
 import java.time.LocalDate;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public interface FetchCategoryService {
     List<Category> fetchByUserIdInRange(Long userId, LocalDate startDate, LocalDate endDate);
+
+    CategoryWithTasks convertToCategoryWithTasks(Category category, LinkedHashSet<TaskWithTimers> taskWithTimers);
 }
--- morib/src/main/java/org/morib/server/domain/category/application/FetchCategoryServiceImpl.java
+++ morib/src/main/java/org/morib/server/domain/category/application/FetchCategoryServiceImpl.java
@@ -1,20 +1,33 @@
 package org.morib.server.domain.category.application;
 
 import lombok.RequiredArgsConstructor;
+import org.morib.server.api.homeView.vo.CategoryWithTasks;
+import org.morib.server.api.homeView.vo.TaskWithTimers;
+import org.morib.server.domain.category.CategoryManager;
 import org.morib.server.domain.category.infra.Category;
 import org.morib.server.domain.category.infra.CategoryRepository;
+import org.morib.server.domain.task.infra.Task;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
 public class FetchCategoryServiceImpl implements FetchCategoryService{
     private final CategoryRepository categoryRepository;
+    private final CategoryManager categoryManager;
+
 
     @Override
     public List<Category> fetchByUserIdInRange(Long userId, LocalDate startDate, LocalDate endDate) {
         return categoryRepository.findByUserIdInRange(userId, startDate, endDate);
+    }
+
+    @Override
+    public CategoryWithTasks convertToCategoryWithTasks(Category category, LinkedHashSet<TaskWithTimers> taskWithTimers) {
+        return CategoryWithTasks.of(category, taskWithTimers);
     }
 }
