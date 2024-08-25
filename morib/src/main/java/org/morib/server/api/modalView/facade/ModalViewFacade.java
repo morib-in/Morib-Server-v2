2a046242156ecab73260e8fc0c05d5309d6eeed5
geniuus <cross_man@naver.com>
2024-08-25 19:43:08 +0900

    Feat [#25] 카테고리 생성 facade 구현

--- morib/src/main/java/org/morib/server/api/modalView/facade/ModalViewFacade.java
+++ morib/src/main/java/org/morib/server/api/modalView/facade/ModalViewFacade.java
@@ -1,10 +1,32 @@
 package org.morib.server.api.modalView.facade;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.morib.server.annotation.Facade;
+import org.morib.server.api.modalView.dto.CreateCategoryRequest;
+import org.morib.server.domain.allowedSite.AllowedSiteManager;
+import org.morib.server.domain.allowedSite.application.CreateAllowedSiteService;
+import org.morib.server.domain.allowedSite.infra.type.OwnerType;
+import org.morib.server.domain.category.CategoryManager;
+import org.morib.server.domain.category.application.CreateCategoryService;
+import org.morib.server.domain.category.infra.Category;
+import org.morib.server.domain.user.application.FetchUserService;
+import org.morib.server.domain.user.infra.User;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @RequiredArgsConstructor
 @Facade
 public class ModalViewFacade {
 
+    private final FetchUserService fetchUserService;
+    private final CreateCategoryService createCategoryService;
+    private final CreateAllowedSiteService createAllowedSiteService;
+
+    @Transactional
+    public void createCategory(Long userId, CreateCategoryRequest createCategoryRequest) {
+        User user = fetchUserService.fetchByUserId(userId);
+        Category createdCategory = createCategoryService.create(createCategoryRequest.name(), createCategoryRequest.startDate(), createCategoryRequest.endDate(), user);
+        createCategoryRequest.allowedSites().stream().map(
+                allowedSite -> createAllowedSiteService.create(allowedSite.getSiteName(), allowedSite.getSiteUrl(), OwnerType.CATEGORY, createdCategory.getId()));
+    }
 }
