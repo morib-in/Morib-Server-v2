c3f01a9caeedae931e2ebc2251f6bc7e56c33cdd
Khyojun <khyojun1104@gmail.com>
2024-08-25 19:50:40 +0900

    Refactor [#24] manager에 manager 어노테이션으로 변경!

--- morib/src/main/java/org/morib/server/domain/todo/TodoManager.java
+++ morib/src/main/java/org/morib/server/domain/todo/TodoManager.java
@@ -1,11 +1,11 @@
 package org.morib.server.domain.todo;
 
 import java.util.Set;
+import org.morib.server.annotation.Manager;
 import org.morib.server.domain.task.infra.Task;
 import org.morib.server.domain.todo.infra.Todo;
-import org.springframework.stereotype.Service;
 
-@Service
+@Manager
 public class TodoManager {
 
     public void updateTask(Todo todo, Set<Task> tasks) {
