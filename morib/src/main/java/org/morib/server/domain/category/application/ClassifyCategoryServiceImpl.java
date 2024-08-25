483b7b0c2cb9565d76c5c116c0c51643d13fcff4
geniuus <cross_man@naver.com>
2024-08-25 16:33:49 +0900

    Refactor [#23] 불필요한 메서드 삭제

--- morib/src/main/java/org/morib/server/domain/category/application/ClassifyCategoryServiceImpl.java
+++ morib/src/main/java/org/morib/server/domain/category/application/ClassifyCategoryServiceImpl.java
@@ -1,27 +1,13 @@
 package org.morib.server.domain.category.application;
 
 import lombok.RequiredArgsConstructor;
-import org.morib.server.api.homeView.vo.CategoriesByDate;
-import org.morib.server.api.homeView.vo.CombinedByDate;
 import org.morib.server.domain.category.CategoryManager;
-import org.morib.server.domain.category.infra.Category;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ClassifyCategoryServiceImpl implements ClassifyCategoryService{
     private final CategoryManager categoryManager;
 
-    @Override
-    public List<CategoriesByDate> classifyByDate(List<Category> categories, LocalDate startDate, LocalDate endDate) {
-        return categoryManager.classifyByDate(categories, startDate, endDate);
-    }
 
-    @Override
-    public List<CombinedByDate> classifyTaskByCategory(List<CategoriesByDate> categories) {
-        return categories.stream().map(categoryManager::classifyTaskByCategory).toList();
-    }
 }
