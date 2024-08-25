b58300101002fb266b0aaa9cd4f96817d955dbdb
geniuus <cross_man@naver.com>
2024-08-25 16:32:57 +0900

    Refactor [#23] 순서가 보장되지 않는 Set -> LinkedHashSet으로 변환하는 메서드 추가

--- morib/src/main/java/org/morib/server/domain/task/application/ClassifyTaskService.java
+++ morib/src/main/java/org/morib/server/domain/task/application/ClassifyTaskService.java
@@ -1,11 +1,15 @@
 package org.morib.server.domain.task.application;
 
-import org.morib.server.api.homeView.vo.TaskWithElapsedTime;
+import org.morib.server.api.homeView.vo.TaskWithTimers;
 import org.morib.server.domain.task.infra.Task;
 
 import java.time.LocalDate;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public interface ClassifyTaskService {
-    List<TaskWithElapsedTime> classifyTimerByTask(LocalDate date, List<Task> tasks);
+    List<TaskWithTimers> classifyTimerByTask(LocalDate date, List<Task> tasks);
+
+    LinkedHashSet<Task> sortTasksByCreatedAt(Set<Task> tasks);
 }
--- morib/src/main/java/org/morib/server/domain/task/application/ClassifyTaskServiceImpl.java
+++ morib/src/main/java/org/morib/server/domain/task/application/ClassifyTaskServiceImpl.java
@@ -2,13 +2,17 @@ package org.morib.server.domain.task.application;
 
 import lombok.RequiredArgsConstructor;
 
-import org.morib.server.api.homeView.vo.TaskWithElapsedTime;
+import org.morib.server.api.homeView.vo.TaskWithTimers;
 import org.morib.server.domain.task.TaskManager;
 import org.morib.server.domain.task.infra.Task;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,7 +20,14 @@ public class ClassifyTaskServiceImpl implements ClassifyTaskService {
     private final TaskManager taskManager;
 
     @Override
-    public List<TaskWithElapsedTime> classifyTimerByTask(LocalDate date, List<Task> tasks) {
+    public List<TaskWithTimers> classifyTimerByTask(LocalDate date, List<Task> tasks) {
         return tasks.stream().map(task -> taskManager.classifyTimerByTask(date, task)).toList();
+    }
+
+    @Override
+    public LinkedHashSet<Task> sortTasksByCreatedAt(Set<Task> tasks) {
+        return tasks.stream()
+                .sorted(Comparator.comparing(Task::getCreatedAt)) // createdAt 필드를 기준으로 정렬
+                .collect(Collectors.toCollection(LinkedHashSet::new)); // LinkedHashSet으로 수집
     }
 }
