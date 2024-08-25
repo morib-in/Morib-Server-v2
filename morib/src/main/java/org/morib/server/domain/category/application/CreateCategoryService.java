6a190e49231688316bf2c2d9287e51c2d57ca231
geniuus <cross_man@naver.com>
2024-08-25 19:41:19 +0900

    Feat [#25] 카테고리 생성 구현

---
+++ morib/src/main/java/org/morib/server/domain/category/application/CreateCategoryService.java
@@ -0,0 +1,10 @@
+package org.morib.server.domain.category.application;
+
+import org.morib.server.domain.category.infra.Category;
+import org.morib.server.domain.user.infra.User;
+
+import java.time.LocalDate;
+
+public interface CreateCategoryService {
+    Category create(String name, LocalDate startDate, LocalDate endDate, User user);
+}
---
+++ morib/src/main/java/org/morib/server/domain/category/application/CreateCategoryServiceImpl.java
@@ -0,0 +1,21 @@
+package org.morib.server.domain.category.application;
+
+import lombok.RequiredArgsConstructor;
+import org.morib.server.domain.category.infra.Category;
+import org.morib.server.domain.category.infra.CategoryRepository;
+import org.morib.server.domain.user.infra.User;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class CreateCategoryServiceImpl implements CreateCategoryService{
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Category create(String name, LocalDate startDate, LocalDate endDate, User user) {
+        return categoryRepository.save(Category.create(name, startDate, endDate, user));
+    }
+
+}
