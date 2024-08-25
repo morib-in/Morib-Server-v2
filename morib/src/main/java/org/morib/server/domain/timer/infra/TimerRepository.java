869a9b426085cb31b64e8ba158312971da1022a2
Khyojun <khyojun1104@gmail.com>
2024-08-25 21:56:38 +0900

    Fix [#28]  총 경과시간 구현 로직 변화에 대한 작업
    - user가 가지고 있는 모든 timer에 대한 elapsedTime 계산
    - 구현 로직 변동에 따른 repository, service 변경

--- morib/src/main/java/org/morib/server/domain/timer/infra/TimerRepository.java
+++ morib/src/main/java/org/morib/server/domain/timer/infra/TimerRepository.java
@@ -1,8 +1,12 @@
 package org.morib.server.domain.timer.infra;
 
+import java.util.List;
+import org.morib.server.domain.user.infra.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TimerRepository extends JpaRepository<Timer, Long> {
+
+    List<Timer> findByUser(User user);
 }
