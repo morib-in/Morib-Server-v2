efd56470df5fae9ee6cd648a3b60eb9983ad461a
geniuus <cross_man@naver.com>
2024-08-28 00:56:31 +0900

    Refactor [#23] CategoryManager Service 단으로 메서드 이동

--- morib/src/main/java/org/morib/server/domain/category/CategoryManager.java
+++ morib/src/main/java/org/morib/server/domain/category/CategoryManager.java
@@ -1,53 +1,8 @@
 package org.morib.server.domain.category;
 
 import org.morib.server.annotation.Manager;
-import org.morib.server.api.homeView.vo.CategoriesByDate;
-import org.morib.server.api.homeView.vo.CategoryWithTasks;
-import org.morib.server.api.homeView.vo.CategoryWithTasksByDate;
-import org.morib.server.domain.category.infra.Category;
-import org.morib.server.domain.task.infra.Task;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Manager
 public class CategoryManager {
 
-    public List<CategoriesByDate> classifyByDate(List<Category> categories, LocalDate startDate, LocalDate endDate) {
-        List<CategoriesByDate> result = new ArrayList<>();
-        for (LocalDate idxDate = startDate; !idxDate.isAfter(endDate); idxDate = idxDate.plusDays(1)) {
-            LocalDate copiedIdxDate = idxDate;
-            result.add(CategoriesByDate.of(copiedIdxDate, categories.stream()
-                            .filter(category -> isInRange(copiedIdxDate, category.getStartDate(), category.getEndDate()))
-                            .collect(Collectors.toList())));
-        }
-        return result;
-    }
-
-    public CategoryWithTasksByDate classifyTaskByCategory(CategoriesByDate categoriesByDate) {
-        LocalDate idxDate = categoriesByDate.getDate();
-        List<CategoryWithTasks> categoryWithTasks = new ArrayList<>();
-        for (Category category : categoriesByDate.getCategories()) {
-            categoryWithTasks.add(CategoryWithTasks.of(category, getTasks(category, idxDate)));
-        }
-        return CategoryWithTasksByDate.of(idxDate, categoryWithTasks);
-    }
-
-    private List<Task> getTasks(Category category, LocalDate idxDate) {
-        List<Task> tasks = new ArrayList<>();
-        for (Task task : category.getTasks()) {
-            if (isInRange(idxDate, task.getStartDate(), task.getEndDate())) {
-                tasks.add(task);
-            }
-        }
-        return tasks;
-    }
-
-    private boolean isInRange(LocalDate idxDate, LocalDate startDate, LocalDate endDate) {
-        return !idxDate.isBefore(startDate) && !idxDate.isAfter(endDate);
-    }
-    
-    
 }
