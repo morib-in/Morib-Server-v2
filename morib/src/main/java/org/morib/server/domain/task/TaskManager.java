b25c372444222f9d5d2385244bdbbeb7e685d4fa
geniuus <cross_man@naver.com>
2024-08-28 00:55:33 +0900

    Refactor [#23] TaskManager Service 단으로 메서드 이동

--- morib/src/main/java/org/morib/server/domain/task/TaskManager.java
+++ morib/src/main/java/org/morib/server/domain/task/TaskManager.java
@@ -11,12 +11,5 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 @Service
 public class TaskManager {
-    public TaskWithTimers classifyTimerByTask(LocalDate date, Task task) {
-        int elapsedTime = task.getTimers().stream()
-                .filter(timer -> timer.getTargetDate().equals(date))
-                .map(Timer::getElapsedTime)
-                .findFirst()
-                .orElse(0); // 일치하는 타이머가 없으면 0으로 설정
-        return new TaskWithTimers(date, task, elapsedTime);
-    }
+
 }
