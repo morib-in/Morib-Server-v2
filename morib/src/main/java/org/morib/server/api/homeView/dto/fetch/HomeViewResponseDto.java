b37d971c24af5a7e8f5d9bbea11c23f6e8e14b35
geniuus <cross_man@naver.com>
2024-08-28 00:58:10 +0900

    Refactor [#23] dto 네이밍 변경에 따른 HomeViewResponseDto 변경 사항

--- morib/src/main/java/org/morib/server/api/homeView/dto/fetch/HomeViewResponseDto.java
+++ morib/src/main/java/org/morib/server/api/homeView/dto/fetch/HomeViewResponseDto.java
@@ -1,14 +1,16 @@
 package org.morib.server.api.homeView.dto.fetch;
 
+import org.morib.server.api.homeView.vo.CombinedCategoryAndTaskInfo;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public record HomeViewResponseDto (
         LocalDate date,
-        List<FetchCombinedDto> fetchCombinedDtos
+        List<CombinedCategoryAndTaskInfo> combinedCategoryAndTaskInfos
 ) {
-    public static HomeViewResponseDto of(LocalDate date, List<FetchCombinedDto> fetchCombinedDtos) {
-        return new HomeViewResponseDto(date, fetchCombinedDtos);
+    public static HomeViewResponseDto of(LocalDate date, List<CombinedCategoryAndTaskInfo> combinedCategoryAndTaskInfos) {
+        return new HomeViewResponseDto(date, combinedCategoryAndTaskInfos);
     }
 }
 
