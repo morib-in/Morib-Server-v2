f06660cb03529010f65a95126cb157195bfeb0ce
geniuus <cross_man@naver.com>
2024-08-28 18:42:17 +0900

    Refactor [#23] DataUtils @Component 제거

--- morib/src/main/java/org/morib/server/global/common/DataUtils.java
+++ morib/src/main/java/org/morib/server/global/common/DataUtils.java
@@ -1,12 +1,10 @@
 package org.morib.server.global.common;
 
-import org.springframework.stereotype.Component;
-
 import java.time.LocalDate;
 
-@Component
 public class DataUtils {
     public static boolean isInRange(LocalDate idxDate, LocalDate startDate, LocalDate endDate) {
         return !idxDate.isBefore(startDate) && !idxDate.isAfter(endDate);
     }
 }
+
