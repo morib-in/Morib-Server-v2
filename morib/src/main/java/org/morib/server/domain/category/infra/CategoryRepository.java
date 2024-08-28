422f4eadfe4ae9137e0b4012f6e968452603b7db
geniuus <cross_man@naver.com>
2024-08-28 21:18:52 +0900

    Hotfix [#23] CategoryRepository @Repository 제거

--- morib/src/main/java/org/morib/server/domain/category/infra/CategoryRepository.java
+++ morib/src/main/java/org/morib/server/domain/category/infra/CategoryRepository.java
@@ -4,12 +4,10 @@ import java.util.Optional;
 import org.morib.server.domain.user.infra.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     @Query("select e from Category e where " +
             "e.user.id = :userId " +
