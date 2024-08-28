3bc89e7741922778ec0d8a1783af7e8230543e7b
Khyojun <nandong5923@naver.com>
2024-08-28 18:41:10 +0900

    Refactor [#37] imageUrl이 사라짐에 따른 리팩터링 수행

--- morib/src/main/java/org/morib/server/api/modalView/vo/AllowSiteForCalledByTask.java
+++ morib/src/main/java/org/morib/server/api/modalView/vo/AllowSiteForCalledByTask.java
@@ -2,13 +2,12 @@ package org.morib.server.api.modalView.vo;
 
 import org.morib.server.domain.allowedSite.infra.AllowedSite;
 
-// imageUrl 없다면 -> calledByOwnerType 으로 변경하여 진행하기
 public record AllowSiteForCalledByTask(
     Long id,
     String name,
     String url
 ) {
-    public static AllowSiteForCalledByTask of(AllowedSite allowedSite) { // 다른 api의 공통된 부분을 찾아서 추후에 수정
+    public static AllowSiteForCalledByTask of(AllowedSite allowedSite) {
         return new AllowSiteForCalledByTask(allowedSite.getId(), allowedSite.getSiteName(),
             allowedSite.getSiteUrl());
     }
