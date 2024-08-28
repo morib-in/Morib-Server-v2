05e596ef46284ce47fa4cdb10646e67005acd682
geniuus <cross_man@naver.com>
2024-08-28 21:09:00 +0900

    Hotfix [#23] 컨플릭트 해결 과정에서 유실된 TaskManager 코드 복구

--- morib/src/main/java/org/morib/server/domain/task/TaskManager.java
+++ morib/src/main/java/org/morib/server/domain/task/TaskManager.java
@@ -1,14 +1,13 @@
 package org.morib.server.domain.task;
 
 import lombok.RequiredArgsConstructor;
-import org.morib.server.api.homeView.vo.TaskWithTimers;
 import org.morib.server.domain.task.infra.Task;
-import org.morib.server.domain.timer.infra.Timer;
 import org.springframework.stereotype.Service;
-import java.time.LocalDate;
-
 
 @RequiredArgsConstructor
 @Service
 public class TaskManager {
+    public void toggleTaskStatus(Task findTask) {
+        findTask.toggleStatus();
+    }
 }
