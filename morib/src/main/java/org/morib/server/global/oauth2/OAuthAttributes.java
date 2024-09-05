2959aec3b3b2b07d5f14a715aeaf0e1f90be9fcf
geniuus <cross_man@naver.com>
2024-09-06 00:07:36 +0900

    Feat [#57] OAuth2UserInfo를 담는 Attributes

---
+++ morib/src/main/java/org/morib/server/global/oauth2/OAuthAttributes.java
@@ -0,0 +1,36 @@
+package org.morib.server.global.oauth2;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.morib.server.domain.user.infra.type.Platform;
+import org.morib.server.global.oauth2.userinfo.GoogleOAuth2UserInfo;
+import org.morib.server.global.oauth2.userinfo.OAuth2UserInfo;
+
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+
+    private String nameAttributeKey;
+    private OAuth2UserInfo oauth2UserInfo;
+
+    @Builder
+    public OAuthAttributes(String nameAttributeKey, OAuth2UserInfo oauth2UserInfo) {
+        this.nameAttributeKey = nameAttributeKey;
+        this.oauth2UserInfo = oauth2UserInfo;
+    }
+
+    public static OAuthAttributes of(Platform platform,
+                                     String userNameAttributeName, Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .nameAttributeKey(userNameAttributeName)
+                .oauth2UserInfo(new GoogleOAuth2UserInfo(attributes))
+                .build();
+    }
+
+}
