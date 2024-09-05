dadb5595eb8c41dccac7dc6d066583a688a6659d
geniuus <cross_man@naver.com>
2024-09-06 00:10:55 +0900

    Feat [#57] OAuth2 플로우를 진행하는 서비스 클래스

---
+++ morib/src/main/java/org/morib/server/global/oauth2/service/CustomOAuth2UserService.java
@@ -0,0 +1,67 @@
+package org.morib.server.global.oauth2.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.morib.server.domain.user.infra.User;
+import org.morib.server.domain.user.infra.UserRepository;
+import org.morib.server.domain.user.infra.type.Platform;
+import org.morib.server.global.oauth2.CustomOAuth2User;
+import org.morib.server.global.oauth2.OAuthAttributes;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        log.info("CustomOAuth2UserService.loadUser() 실행 - OAuth2 로그인 요청 진입");
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        Platform platform = getSocialType(registrationId);
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        OAuthAttributes extractAttributes = OAuthAttributes.of(platform, userNameAttributeName, attributes);
+        User createdUser = getUser(extractAttributes, platform);
+        return new CustomOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(createdUser.getRole().getKey())),
+                attributes,
+                extractAttributes.getNameAttributeKey(),
+                createdUser.getRole(),
+                createdUser.getId()
+        );
+    }
+
+    private Platform getSocialType(String registrationId) {
+        return Platform.GOOGLE;
+    }
+
+    private User getUser(OAuthAttributes attributes, Platform platform) {
+        User findUser = userRepository.findByPlatformAndSocialId(platform,
+                attributes.getOauth2UserInfo().getId()).orElse(null);
+
+        if(findUser == null) {
+            return saveUser(attributes, platform);
+        }
+        return findUser;
+    }
+
+    private User saveUser(OAuthAttributes attributes, Platform platform) {
+        User createdUser = User.createByOAuth2UserInfo(platform, attributes.getOauth2UserInfo());
+        return userRepository.saveAndFlush(createdUser);
+    }
+}
