0134877eaa202f47d20d13c158ff9cfc42937182
geniuus <cross_man@naver.com>
2024-09-06 00:08:02 +0900

    Feat [#57] DefaultOAuth2User를 커스텀한 CustomOAuth2User

---
+++ morib/src/main/java/org/morib/server/global/oauth2/CustomOAuth2User.java
@@ -0,0 +1,24 @@
+package org.morib.server.global.oauth2;
+
+import lombok.Getter;
+import org.morib.server.domain.user.infra.type.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+public class CustomOAuth2User extends DefaultOAuth2User {
+
+    private Role role;
+    private Long userId;
+
+    public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities,
+                            Map<String, Object> attributes, String nameAttributeKey,
+                            Role role, Long userId) {
+        super(authorities, attributes, nameAttributeKey);
+        this.role = role;
+        this.userId = userId;
+    }
+}
