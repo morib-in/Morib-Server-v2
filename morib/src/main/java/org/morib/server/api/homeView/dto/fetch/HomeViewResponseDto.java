2657c30fe027249443a04a1be0f74380c50dab47
geniuus <cross_man@naver.com>
2024-09-26 23:37:15 +0900

    Feat [#62] 홈뷰 전체조회 api @JsonProperty 사용
    combinedCategoryAndTaskInfos -> categories

--- morib/src/main/java/org/morib/server/api/homeView/dto/fetch/HomeViewResponseDto.java
+++ morib/src/main/java/org/morib/server/api/homeView/dto/fetch/HomeViewResponseDto.java
@@ -1,5 +1,6 @@
 package org.morib.server.api.homeView.dto.fetch;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.morib.server.api.homeView.vo.CombinedCategoryAndTaskInfo;
 
 import java.time.LocalDate;
@@ -7,6 +8,7 @@ import java.util.List;
 
 public record HomeViewResponseDto (
         LocalDate date,
+        @JsonProperty("categories")
         List<CombinedCategoryAndTaskInfo> combinedCategoryAndTaskInfos
 ) {
     public static HomeViewResponseDto of(LocalDate date, List<CombinedCategoryAndTaskInfo> combinedCategoryAndTaskInfos) {
