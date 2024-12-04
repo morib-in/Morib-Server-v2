be18d781508dd0bca7815912f09869ac02379c05
geniuus <cross_man@naver.com>
2024-12-04 19:31:32 +0900

    Refactor [#79] 메소드, dto 네이밍 구체화

--- morib/src/main/java/org/morib/server/domain/relationship/application/FetchFriendsService.java
+++
@@ -1,9 +0,0 @@
-package org.morib.server.domain.relationship.application;
-
-import org.morib.server.domain.relationship.infra.Relationship;
-
-import java.util.List;
-
-public interface FetchFriendsService {
-    List<Relationship> fetchConnectedRelationship(Long userId);
-}
