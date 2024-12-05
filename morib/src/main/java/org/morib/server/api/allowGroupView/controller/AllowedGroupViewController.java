3c4aff458df7b87eb6a5134dc572b79cf4b85ee4
Khyojun <khyojun1104@gmail.com>
2024-12-06 00:20:09 +0900

    Feat [#87] 허용 서비스 세트 정보 변경 api 계층들 구현 진행

--- morib/src/main/java/org/morib/server/api/allowGroupView/controller/AllowedGroupViewController.java
+++ morib/src/main/java/org/morib/server/api/allowGroupView/controller/AllowedGroupViewController.java
@@ -1,16 +1,21 @@
 package org.morib.server.api.allowGroupView.controller;
 
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.morib.server.api.allowGroupView.dto.AddAllowSiteInAllowGroupRequestDto;
 import org.morib.server.api.allowGroupView.facade.AllowedGroupViewFacade;
 import org.morib.server.global.common.BaseResponse;
+import org.morib.server.global.exception.InvalidQueryParameterException;
+import org.morib.server.global.message.ErrorMessage;
 import org.morib.server.global.message.SuccessMessage;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,17 +26,31 @@ public class AllowedGroupViewController {
     private final AllowedGroupViewFacade allowedGroupViewFacade;
 
     @DeleteMapping("/{groupId}")
-    public ResponseEntity<BaseResponse<?>> deleteAllowServiceSet(@PathVariable Long groupId){
+    public ResponseEntity<BaseResponse<?>> deleteAllowServiceSet(@PathVariable Long groupId) {
         allowedGroupViewFacade.deleteAllowServiceSet(groupId);
         return ResponseEntity.ok(BaseResponse.of(SuccessMessage.SUCCESS));
     }
 
     @PostMapping("/allowedSite")
     public ResponseEntity<BaseResponse<?>> addAllowedSite(@RequestBody
-        AddAllowSiteInAllowGroupRequestDto addAllowSiteInAllowGroupRequestDto){
+    AddAllowSiteInAllowGroupRequestDto addAllowSiteInAllowGroupRequestDto) {
         allowedGroupViewFacade.addAllowedSite(addAllowSiteInAllowGroupRequestDto);
         return ResponseEntity.ok(BaseResponse.of(SuccessMessage.SUCCESS));
     }
 
+    @PatchMapping("/{groupId}")
+    public ResponseEntity<BaseResponse<?>> updateAllowedGroup(@PathVariable Long groupId,
+        @RequestParam(required = false) String colorCode,
+        @RequestParam(required = false) String name) {
+        if (isParamAllNull(colorCode, name))
+            throw new InvalidQueryParameterException(ErrorMessage.BAD_REQUEST);
+
+        allowedGroupViewFacade.updateAllowedGroup(groupId, colorCode, name);
+        return ResponseEntity.ok(BaseResponse.of(SuccessMessage.SUCCESS));
+    }
+
+    private boolean isParamAllNull(String colorCode, String name) {
+        return Objects.isNull(colorCode) && Objects.isNull(name);
+    }
 
 }
