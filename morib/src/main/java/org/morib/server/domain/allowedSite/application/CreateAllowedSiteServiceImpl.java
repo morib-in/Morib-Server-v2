a0b3ab7e0215f980364df9f348955150e705ffd7
Khyojun <khyojun1104@gmail.com>
2024-12-05 21:55:36 +0900

    Feat [#85] 연관관계의 주인인 allowSite에서 allowedGroup 조종하기 위해 먼저 넣어주는 로직 작성

--- morib/src/main/java/org/morib/server/domain/allowedSite/application/CreateAllowedSiteServiceImpl.java
+++ morib/src/main/java/org/morib/server/domain/allowedSite/application/CreateAllowedSiteServiceImpl.java
@@ -1,6 +1,8 @@
 package org.morib.server.domain.allowedSite.application;
 
 import lombok.RequiredArgsConstructor;
+import org.morib.server.domain.allowedGroup.infra.AllowedGroup;
+import org.morib.server.domain.allowedSite.application.dto.AddAllowSiteInAllowGroupServiceDto;
 import org.morib.server.domain.allowedSite.infra.AllowedSite;
 import org.morib.server.domain.allowedSite.infra.AllowedSiteRepository;
 import org.springframework.stereotype.Service;
@@ -13,5 +15,10 @@ public class CreateAllowedSiteServiceImpl implements CreateAllowedSiteService {
     @Override
     public AllowedSite create(String siteName, String siteUrl) {
         return allowedSiteRepository.save(AllowedSite.create(siteName, siteUrl));
+    }
+
+    @Override
+    public AllowedSite create(AddAllowSiteInAllowGroupServiceDto dto) {
+        return allowedSiteRepository.save(AllowedSite.create(dto.site(), dto.name(), dto.findAllowedGroup()));
     }
 }
