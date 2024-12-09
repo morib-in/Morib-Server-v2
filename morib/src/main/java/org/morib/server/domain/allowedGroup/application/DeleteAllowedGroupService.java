36ffcc5bc80a1c4cfc62bf7da958d3642dc6e424
Khyojun <khyojun1104@gmail.com>
2024-12-10 00:06:15 +0900

    Fix [#85] 클래스 역할 더욱 세분화해서 진행될 수 있도록 수정 fetchAllowedGroupService 로 분리

--- morib/src/main/java/org/morib/server/domain/allowedGroup/application/DeleteAllowedGroupService.java
+++ morib/src/main/java/org/morib/server/domain/allowedGroup/application/DeleteAllowedGroupService.java
@@ -3,5 +3,4 @@ package org.morib.server.domain.allowedGroup.application;
 public interface DeleteAllowedGroupService {
     void deleteAllowedGroupById(Long groupId);
 
-    AllowedGroup findById(Long groupId);
 }
