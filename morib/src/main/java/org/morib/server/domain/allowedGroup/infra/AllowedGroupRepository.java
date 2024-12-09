413352a501652416e493186e1d60856435b88e0a
Khyojun <khyojun1104@gmail.com>
2024-12-09 23:46:26 +0900

    Fix [#83] 깜빡하고 넣은 method 삭제

--- morib/src/main/java/org/morib/server/domain/allowedGroup/infra/AllowedGroupRepository.java
+++ morib/src/main/java/org/morib/server/domain/allowedGroup/infra/AllowedGroupRepository.java
@@ -3,6 +3,5 @@ package org.morib.server.domain.allowedGroup.infra;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AllowedGroupRepository extends JpaRepository<AllowedGroup, Long> {
-    void deleteById(Long groupId);
 
 }
