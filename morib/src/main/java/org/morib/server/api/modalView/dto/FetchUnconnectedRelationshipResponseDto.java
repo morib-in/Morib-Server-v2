52157fb766d96451db0e6df099faba18ca6e312e
geniuus <cross_man@naver.com>
2025-01-13 17:51:14 +0900

    Refactor [#101] Dto 네이밍 변경

--- morib/src/main/java/org/morib/server/api/modalView/dto/FetchUnconnectedRelationshipResponseDto.java
+++ morib/src/main/java/org/morib/server/api/modalView/dto/FetchUnconnectedRelationshipResponseDto.java
@@ -3,10 +3,10 @@ package org.morib.server.api.modalView.dto;
 import java.util.List;
 
 public record FetchUnconnectedRelationshipResponseDto(
-        List<FetchRelationshipResponseDto> send,
-        List<FetchRelationshipResponseDto> receive
+        List<FetchRelationshipRequestResponseDto> send,
+        List<FetchRelationshipRequestResponseDto> receive
 ) {
-    public static FetchUnconnectedRelationshipResponseDto of (List<FetchRelationshipResponseDto> send, List<FetchRelationshipResponseDto> receive) {
+    public static FetchUnconnectedRelationshipResponseDto of (List<FetchRelationshipRequestResponseDto> send, List<FetchRelationshipRequestResponseDto> receive) {
         return new FetchUnconnectedRelationshipResponseDto(send, receive);
     }
 }
