a5a6d849e4f6203b8058f4eb44353d77cd6e0ae4
geniuus <cross_man@naver.com>
2025-01-13 18:00:55 +0900

    Feat [#102] 로그인 성공 시 redirect 하는 코드 추가

--- morib/src/main/java/org/morib/server/global/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ morib/src/main/java/org/morib/server/global/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -5,14 +5,16 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.morib.server.domain.user.UserManager;
+import org.morib.server.domain.user.application.service.FetchUserService;
 import org.morib.server.domain.user.infra.User;
 import org.morib.server.domain.user.infra.UserRepository;
 import org.morib.server.domain.user.infra.type.Role;
 import org.morib.server.global.common.ApiResponseUtil;
+import org.morib.server.global.common.SecretProperties;
 import org.morib.server.global.common.TokenResponseDto;
 import org.morib.server.global.exception.NotFoundException;
 import org.morib.server.global.exception.UnauthorizedException;
@@ -21,12 +23,16 @@ import org.morib.server.global.message.ErrorMessage;
 import org.morib.server.global.message.SuccessMessage;
 import org.morib.server.global.oauth2.CustomOAuth2User;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
+import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-import static org.morib.server.global.common.Constants.REFRESH_TOKEN_SUBJECT;
+import static org.morib.server.global.common.Constants.*;
 
 @Slf4j
 @Component
@@ -34,33 +40,60 @@ import static org.morib.server.global.common.Constants.REFRESH_TOKEN_SUBJECT;
 public class  OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtService jwtService;
+    private final FetchUserService fetchUserService;
+    private final UserManager userManager;
     private final UserRepository userRepository;
     private final ObjectMapper objectMapper;
+    private final OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+    private final SecretProperties secretProperties;
 
     @Override
     @Transactional
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("OAuth2 Login 성공!");
+        boolean isSignUp = false;
         try {
             CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
             if(oAuth2User.getRole() == Role.GUEST) { // 회원 가입
                 User findUser = userRepository.findById(oAuth2User.getUserId())
                         .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND));
                 findUser.authorizeUser();
+                isSignUp = true;
             }
-            loginSuccess(response, oAuth2User);
+            loginSuccess(response, oAuth2User, isSignUp);
         } catch (Exception e) {
             throw new UnauthorizedException(ErrorMessage.INVALID_TOKEN);
         }
+
     }
 
-    private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
+    private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User, boolean isSignUp) throws IOException {
         String accessToken = jwtService.createAccessToken(oAuth2User.getUserId());
         String refreshToken = jwtService.createRefreshToken();
         jwtService.updateRefreshToken(oAuth2User.getUserId(), refreshToken);
-        response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.addCookie(new Cookie(REFRESH_TOKEN_SUBJECT, refreshToken));
-        response.getWriter().write(objectMapper.writeValueAsString(ApiResponseUtil.success(SuccessMessage.SUCCESS, TokenResponseDto.of(accessToken, oAuth2User.getUserId())).getBody()));
+        userManager.updateSocialRefreshToken(
+                fetchUserService.fetchByUserId(
+                        oAuth2User.getUserId()), getSocialRefreshTokenByAuthorizedClient(oAuth2User.getRegistrationId(), oAuth2User.getPrincipalName()));
+        StringBuilder redirectUri = new StringBuilder(secretProperties.getClientRedirectUriProd());
+        redirectUri.append(IS_SIGN_UP_QUERYSTRING).append(isSignUp);
+        response.addCookie(getCookieForToken(ACCESS_TOKEN_SUBJECT, accessToken));
+        response.addCookie(getCookieForToken(REFRESH_TOKEN_SUBJECT, refreshToken));
+        response.sendRedirect(redirectUri.toString());
+    }
+
+    private String getSocialRefreshTokenByAuthorizedClient(String registrationId, String principalName) {
+        OAuth2AuthorizedClient user = oAuth2AuthorizedClientService.loadAuthorizedClient(registrationId, principalName);
+        OAuth2RefreshToken refreshToken = user.getRefreshToken();
+        return refreshToken.getTokenValue();
+    }
+
+    private Cookie getCookieForToken(String sub, String token) {
+        Cookie cookie = new Cookie(sub, token);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setDomain(secretProperties.getClientRedirectUriProd()); // 쿠키를 사용할 도메인
+        cookie.setAttribute("SameSite", "None");
+        return cookie;
     }
 }
