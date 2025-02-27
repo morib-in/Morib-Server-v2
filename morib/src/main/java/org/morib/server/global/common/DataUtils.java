c6a117befebb0945f5be7e468ec98779938b71ee
geniuus <cross_man@naver.com>
2025-02-27 22:34:34 +0900

    Refactor [#125] 쿠키 설정 로직

--- morib/src/main/java/org/morib/server/global/common/DataUtils.java
+++ morib/src/main/java/org/morib/server/global/common/DataUtils.java
@@ -1,10 +1,34 @@
 package org.morib.server.global.common;
 
+import jakarta.servlet.http.Cookie;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDate;
 
+@Slf4j
+@RequiredArgsConstructor
+@Component
 public class DataUtils {
     public static boolean isInRange(LocalDate idxDate, LocalDate startDate, LocalDate endDate) {
         return !idxDate.isBefore(startDate) && !idxDate.isAfter(endDate);
+    }
+    public Cookie getCookieForToken(String sub, String token) {
+        log.info("getCookieForToken 진입.");
+        Cookie cookie = new Cookie(sub, token);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setDomain("morib.in"); // 쿠키를 사용할 도메인
+        cookie.setAttribute("SameSite", "None");
+        log.info("getCookieForToken 완료.");
+        log.info("Token Cookie: name={}, value={}, domain={}, path={}",
+                cookie.getName(),
+                cookie.getValue(),
+                cookie.getDomain(),
+                cookie.getPath());
+        return cookie;
     }
 }
 
