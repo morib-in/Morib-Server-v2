fe157cfafad1980fed66380e1ea48baabc39d027
geniuus <cross_man@naver.com>
2025-03-12 21:58:33 +0900

    Refactor [#149] GlobalExceptionHandler 에러메시지 커스텀할 수 있도록 변경

--- morib/src/main/java/org/morib/server/global/exception/GlobalExceptionHandler.java
+++ morib/src/main/java/org/morib/server/global/exception/GlobalExceptionHandler.java
@@ -16,25 +16,25 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     protected ResponseEntity<BaseResponse<?>> handleNotFoundException(final NotFoundException e) {
         log.error(">>> handle: NotFoundException ", e);
-        return ApiResponseUtil.failure(ErrorMessage.NOT_FOUND);
+        return ApiResponseUtil.failure(e.getErrorMessage());
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     protected ResponseEntity<BaseResponse<?>> handleUnauthorizedException(final UnauthorizedException e) {
         log.error(">>> handle: UnauthorizedException ", e);
-        return ApiResponseUtil.failure(ErrorMessage.UNAUTHORIZED);
+        return ApiResponseUtil.failure(e.getErrorMessage());
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     protected ResponseEntity<BaseResponse<?>> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
         log.error(">>> handle: HttpRequestMethodNotSupportedException ", e);
-        return ApiResponseUtil.failure(ErrorMessage.METHOD_NOT_ALLOWED);
+        return ApiResponseUtil.failure(e.getErrorMessage());
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     protected ResponseEntity<BaseResponse<?>> handleMethodArgumentTypeMismatchException(final MethodArgumentTypeMismatchException e) {
         log.error(">>> handle: MethodArgumentTypeMismatchException ", e);
-        return ApiResponseUtil.failure(ErrorMessage.TYPE_MISMATCH);
+        return ApiResponseUtil.failure(e.getErrorMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
@@ -46,25 +46,25 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidURLException.class)
     protected ResponseEntity<BaseResponse<?>> handleInvalidURLException(final InvalidURLException e) {
         log.error(">>> handle: InvalidURLException ", e);
-        return ApiResponseUtil.failure(ErrorMessage.TYPE_MISMATCH);
+        return ApiResponseUtil.failure(e.getErrorMessage());
     }
 
     @ExceptionHandler(InvalidQueryParameterException.class)
     protected ResponseEntity<BaseResponse<?>> handleInvalidQueryParameterException(final InvalidQueryParameterException e) {
         log.error(">>> handle: InvalidQueryParameterException ", e);
-        return ApiResponseUtil.failure(ErrorMessage.BAD_REQUEST);
+        return ApiResponseUtil.failure(e.getErrorMessage());
     }
 
     @ExceptionHandler(AlreadyFriendException.class)
     protected ResponseEntity<BaseResponse<?>> handleAlreadyFriendException(final AlreadyFriendException e) {
         log.error(">>> handle: AlreadyFriendException ", e);
-        return ApiResponseUtil.failure(ErrorMessage.ALREADY_FRIEND);
+        return ApiResponseUtil.failure(e.getErrorMessage());
     }
 
     @ExceptionHandler(AlreadyFriendRequestException.class)
     protected ResponseEntity<BaseResponse<?>> handleAlreadyFriendRequestException(final AlreadyFriendRequestException e) {
         log.error(">>> handle: AlreadyFriendRequestException ", e);
-        return ApiResponseUtil.failure(ErrorMessage.ALREADY_FRIEND_REQUEST);
+        return ApiResponseUtil.failure(e.getErrorMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -76,19 +76,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SSEConnectionException.class)
     protected ResponseEntity<BaseResponse<?>> handleSSEConnectionException(final SSEConnectionException e) {
         log.error(">>> handle: SSEConnectionException ", e);
-        return ApiResponseUtil.failure(ErrorMessage.SSE_CONNECT_FAILED);
+        return ApiResponseUtil.failure(e.getErrorMessage());
     }
 
     @ExceptionHandler(DuplicateResourceException.class)
     protected ResponseEntity<BaseResponse<?>> handleDuplicateResourceException(final DuplicateResourceException e) {
         log.error(">>> handle: DuplicateResourceException ", e);
-        return ApiResponseUtil.failure(ErrorMessage.DUPLICATE_RESOURCE);
+        return ApiResponseUtil.failure(e.getErrorMessage());
     }
 
     @ExceptionHandler(InvalidTaskInTodoException.class)
     protected ResponseEntity<BaseResponse<?>> handleInvalidTaskInTodoException(final InvalidTaskInTodoException e) {
         log.error(">>> handle: InvalidTaskInTodoException ", e);
-        return ApiResponseUtil.failure(ErrorMessage.INVALID_TASK_IN_TODO);
+        return ApiResponseUtil.failure(e.getErrorMessage());
     }
 }
 
