439e54a3b602e0d6d873edbd09a50089469a6a75
geniuus <cross_man@naver.com>
2025-03-22 15:01:19 +0900

    Refactor [#161] dto id -> userId로 변경

--- morib/src/main/java/org/morib/server/global/sse/api/UserInfoDtoForSseUserInfoWrapper.java
+++ morib/src/main/java/org/morib/server/global/sse/api/UserInfoDtoForSseUserInfoWrapper.java
@@ -1,12 +1,12 @@
 package org.morib.server.global.sse.api;
 
 public record UserInfoDtoForSseUserInfoWrapper(
-        Long id,
+        Long userId,
         int elapsedTime,
         String runningCategoryName,
         Long taskId
 ) {
-    public static UserInfoDtoForSseUserInfoWrapper of(Long id, int elapsedTime, String runningCategoryName, Long taskId) {
-        return new UserInfoDtoForSseUserInfoWrapper(id, elapsedTime, runningCategoryName, taskId);
+    public static UserInfoDtoForSseUserInfoWrapper of(Long userId, int elapsedTime, String runningCategoryName, Long taskId) {
+        return new UserInfoDtoForSseUserInfoWrapper(userId, elapsedTime, runningCategoryName, taskId);
     }
 }
