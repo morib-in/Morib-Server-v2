5b4f57ecab1ad8d5a183c5fff29dc8569b19ac7c
geniuus <cross_man@naver.com>
2025-03-22 15:04:10 +0900

    Refactor [#161] SSE와 유저정보 분리로 인한 getter 변경사항

--- morib/src/main/java/org/morib/server/global/sse/application/service/SseService.java
+++ morib/src/main/java/org/morib/server/global/sse/application/service/SseService.java
@@ -1,25 +1,15 @@
 package org.morib.server.global.sse.application.service;
 
 import lombok.RequiredArgsConstructor;
-import org.morib.server.domain.relationship.application.FetchRelationshipService;
-import org.morib.server.domain.relationship.infra.Relationship;
 import org.morib.server.global.exception.SSEConnectionException;
 import org.morib.server.global.message.ErrorMessage;
-import org.morib.server.global.sse.api.UserInfoDtoForSseUserInfoWrapper;
-import org.morib.server.global.sse.application.event.SseDisconnectEvent;
 import org.morib.server.global.sse.application.repository.SseRepository;
-import org.springframework.context.event.EventListener;
+import org.morib.server.global.sse.application.repository.SseUserInfoWrapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
-
-import static org.morib.server.global.common.Constants.SSE_EVENT_CONNECT;
 
 @Service
 @RequiredArgsConstructor
@@ -32,16 +22,14 @@ public class SseService {
     }
 
     public SseEmitter add(Long userId, SseEmitter emitter) {
-        return sseRepository.add(userId, emitter, 0, "", null);
+        return sseRepository.addSseEmitter(userId, emitter);
     }
 
-    public void saveSseUserInfo(Long userId, SseEmitter emitter, UserInfoDtoForSseUserInfoWrapper calculatedSseUserInfoWrapper) {
-        sseRepository.add(
-                userId,
-                emitter,
-                calculatedSseUserInfoWrapper.elapsedTime(),
-                calculatedSseUserInfoWrapper.runningCategoryName(),
-                calculatedSseUserInfoWrapper.taskId());
+    public void removeExistingEmitter(Long userId) {
+        sseRepository.removeExistingEmitter(userId);
+    }
+    public void saveSseUserInfoWrapper(Long userId, SseUserInfoWrapper calculatedSseUserInfoWrapper) {
+        sseRepository.mergeUserInfoWrapper(userId, calculatedSseUserInfoWrapper);
     }
 
     public List<SseEmitter> fetchConnectedSseEmittersById(List<Long> ids) {
@@ -58,8 +46,8 @@ public class SseService {
     // SseEmitter hashMap에 들어있는 친구들의 카테고리 이름 조회 (온라인인 친구만 조회됨)
     public String fetchFriendsRunningCategoryNameBySseEmitters(Long userId) {
         try {
-            if (SseRepository.emitters.containsKey(userId)) {
-                return SseRepository.emitters.get(userId).getRunningCategoryName();
+            if (sseRepository.getConnectedUserIds().contains(userId)) {
+                return sseRepository.getSseUserInfoWrapperById(userId).getRunningCategoryName();
             }
         } catch (SSEConnectionException e) {
             throw new SSEConnectionException(ErrorMessage.SSE_CONNECT_FAILED);
