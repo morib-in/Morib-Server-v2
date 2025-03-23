fb689d86f7575779d228e4b4bfa6423f6f2214e8
geniuus <cross_man@naver.com>
2025-03-23 19:52:36 +0900

    Feat [#167] 할일 카드 수정 api 더티체킹을 활용한 변경사항

--- morib/src/main/java/org/morib/server/domain/task/TaskManager.java
+++ morib/src/main/java/org/morib/server/domain/task/TaskManager.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.morib.server.annotation.Manager;
 import org.morib.server.domain.task.infra.Task;
 
+import java.time.LocalDate;
+
 @RequiredArgsConstructor
 @Manager
 public class TaskManager {
     public void toggleTaskStatus(Task findTask) {
         findTask.toggleStatus();
+    }
+    public void updateTask(Task findTask, String name, LocalDate startDate, LocalDate endDate) {
+        findTask.updateName(name, startDate, endDate);
     }
 }
