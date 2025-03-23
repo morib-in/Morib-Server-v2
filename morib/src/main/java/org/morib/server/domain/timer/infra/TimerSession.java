423456777f461ba280eca85e42ade81f018584f2
geniuus <cross_man@naver.com>
2025-03-23 18:53:45 +0900

    Feat [#165] TimerSession Entity

---
+++ morib/src/main/java/org/morib/server/domain/timer/infra/TimerSession.java
@@ -0,0 +1,46 @@
+package org.morib.server.domain.timer.infra;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.morib.server.global.common.BaseTimeEntity;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TimerSession extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "timer_session_id")
+    private Long id;
+    @Column(nullable = false)
+    private Long userId;
+    @Column(nullable = false)
+    private String runningCategoryName;
+    @Column(nullable = false)
+    private Long taskId;
+    @Column(nullable = false)
+    private int elapsedTime;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TimerStatus timerStatus;
+    @Column(nullable = false)
+    LocalDate targetDate;
+
+    public static TimerSession create(Long userId, String runningCategoryName, Long taskId, int elapsedTime, TimerStatus timerStatus, LocalDate targetDate) {
+        return TimerSession.builder()
+                .userId(userId)
+                .runningCategoryName(runningCategoryName)
+                .taskId(taskId)
+                .elapsedTime(elapsedTime)
+                .timerStatus(timerStatus)
+                .targetDate(targetDate)
+                .build();
+    }
+}
