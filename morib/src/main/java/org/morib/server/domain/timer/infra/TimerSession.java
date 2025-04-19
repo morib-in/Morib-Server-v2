b74c9ae0d011e8d973f77249502d7ca180222392
geniuus <cross_man@naver.com>
2025-04-19 15:22:55 +0900

    Feat [#189] TimerSession 도메인 로직 추가

--- morib/src/main/java/org/morib/server/domain/timer/infra/TimerSession.java
+++ morib/src/main/java/org/morib/server/domain/timer/infra/TimerSession.java
@@ -2,9 +2,11 @@ package org.morib.server.domain.timer.infra;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.morib.server.domain.task.infra.Task;
 import org.morib.server.global.common.BaseTimeEntity;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -12,6 +14,8 @@ import java.time.LocalDate;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "timer_session",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "targetDate"})})
 public class TimerSession extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,33 +25,65 @@ public class TimerSession extends BaseTimeEntity {
     private Long userId;
     @Column(nullable = false)
     private String runningCategoryName;
-    @Column(nullable = false)
-    private Long taskId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "selected_task_id")
+    private Task selectedTask;
     @Column(nullable = false)
     private int elapsedTime;
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private TimerStatus timerStatus;
+    private TimerStatus timerStatus = TimerStatus.PAUSED;
     @Column(nullable = false)
-    LocalDate targetDate;
+    private LocalDate targetDate;
+    private LocalDateTime lastCalculatedAt;
+    private LocalDateTime lastHeartbeatAt;
 
-    public static TimerSession create(Long userId, String runningCategoryName, Long taskId, int elapsedTime, TimerStatus timerStatus, LocalDate targetDate) {
+    public static TimerSession create(Long userId, String runningCategoryName, Task selectedTask, int elapsedTime, TimerStatus timerStatus, LocalDate targetDate) {
         return TimerSession.builder()
                 .userId(userId)
                 .runningCategoryName(runningCategoryName)
-                .taskId(taskId)
+                .selectedTask(selectedTask)
                 .elapsedTime(elapsedTime)
                 .timerStatus(timerStatus)
                 .targetDate(targetDate)
                 .build();
     }
-}
+
     public void run(LocalDateTime now) {
         this.timerStatus = TimerStatus.RUNNING;
         this.lastCalculatedAt = now;
         this.lastHeartbeatAt = now;
     }
+
+    public void pause(int calculateElapsedTime, LocalDateTime now) {
+        if (this.timerStatus != TimerStatus.RUNNING || this.lastCalculatedAt == null) return; // 실행 중이 아닐 때 무시
+        this.elapsedTime += calculateElapsedTime;
+        this.timerStatus = TimerStatus.PAUSED;
+        this.lastCalculatedAt = now;
+        this.lastHeartbeatAt = now;
+    }
+
     public void updateHeartbeat(LocalDateTime now) {
         if (this.timerStatus != TimerStatus.RUNNING) return;
         this.lastHeartbeatAt = now;
     }
+
+    public void update(String runningCategoryName, Task selectedTask, int elapsedTime, TimerStatus timerStatus, LocalDate targetDate) {
+        this.runningCategoryName = runningCategoryName;
+        this.selectedTask = selectedTask;
+        this.elapsedTime = elapsedTime;
+        this.timerStatus = timerStatus;
+        this.targetDate = targetDate;
+        this.lastCalculatedAt = null;
+        this.lastHeartbeatAt = null;
+    }
+
+    public TimerSession handleCalledByClientFetch(int calculatedElapsedTime, LocalDateTime now) {
+        this.elapsedTime += calculatedElapsedTime;
+        this.lastCalculatedAt = now;
+        this.lastHeartbeatAt = now;
+        return this;
+    }
+
+}
+
